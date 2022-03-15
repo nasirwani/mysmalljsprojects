9ce966ca1bdbc83fadf9fe732caf27c45912b98e
Nasir <waninasie@gmail.com>
2022-03-15 17:35:09 +0530

    dayoftheweek

--- myjs.js
+++ myjs.js
@@ -1,23 +1,34 @@
-let mybtn=document.getElementById('btn');
-myoutput=document.getElementById('output');
-let number=Math.floor(Math.random()*100);
-mytext=document.getElementById('num');
-let mycount=0;
-mybtn.addEventListener('click',function(){
-    let myinput=mytext.value;
-    if(myinput==number){
-        myoutput.innerHTML=`you guessed the right number,and you guessed it in ${mycount} attempts`;
-        mycount+=1;
-    }
-    else if(myinput<number){
-        myoutput.innerHTML=`your number is too low`;
-        mycount+=1
-        
-    }
-    else if(myinput>number){
-        myoutput.innerHTML=`your number is too high`
-        mycount+=1;
-    }
-    mytext.value=' ';
-  
-});
+let myday=document.getElementById('day');
+let myyear=document.getElementById('year');
+//gives date 
+let d= new Date();
+//fetch day starts from 0;
+let days=d.getDay();
+//fetches month and current time
+nw=d.toLocaleString();
+
+switch(days){
+    case 0:
+        days='Sunday';
+        break;
+    case 1:
+        days='Monday';
+        break;
+    case 2:
+        days='Tuesday';
+        break;
+    case 3:
+        days='Wednesday';
+        break;
+    case 4:
+        days='Thursday';
+        break;
+    case 5:
+        days='Friday';
+        break;
+    case 6:
+        days='Saturday';
+        break;
+}
+myday.innerHTML=days;
+myyear.innerHTML=nw;
