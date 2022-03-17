1d569dbbc74fbd5e75fd51b47d2c8664efd20d99
Nasir <waninasie@gmail.com>
2022-03-17 10:39:34 +0530

    heightconverter

--- myjs.js
+++ myjs.js
@@ -1,34 +1,12 @@
-let myday=document.getElementById('day');
-let myyear=document.getElementById('year');
-//gives date 
-let d= new Date();
-//fetch day starts from 0;
-let days=d.getDay();
-//fetches month and current time
-nw=d.toLocaleString();
-
-switch(days){
-    case 0:
-        days='Sunday';
-        break;
-    case 1:
-        days='Monday';
-        break;
-    case 2:
-        days='Tuesday';
-        break;
-    case 3:
-        days='Wednesday';
-        break;
-    case 4:
-        days='Thursday';
-        break;
-    case 5:
-        days='Friday';
-        break;
-    case 6:
-        days='Saturday';
-        break;
-}
-myday.innerHTML=days;
-myyear.innerHTML=nw;
+let mymeter=document.getElementById('meter');
+let myfeet=document.getElementById('feet');
+let myoutput=document.getElementById('output')
+mymeter.addEventListener('click',function(){
+    let myHeightInMeters=document.getElementById('len').value
+    console.log(myHeightInMeters)
+    myoutput.innerHTML=myHeightInMeters/3.21+' '+'Meters';
+})
+myfeet.addEventListener('click',function(){
+    myHeightInFeets=document.getElementById('len').value;
+    myoutput.innerHTML=myHeightInFeets*3.21+" "+ 'Feets';
+})
